1fd8230155833dc4705a4db51c5c1e2d8d87158a
John Asmuth <jasmuth@gmail.com>
2008-12-24 00:47:01 +0000

    commented RTDP planner

--- libcrl/include/crl/rtdp.hpp
+++ libcrl/include/crl/rtdp.hpp
@@ -12,28 +12,70 @@
 
 namespace crl {
 
+/**
+ * A planner that uses RTDP
+ */
 class _RTDPPlanner : public _Planner {
 protected:
+	/**
+	 * The domain describes what valid states and actions are.
+	 */
 	Domain _domain;
+	/**
+	 * The MDP to plan with
+	 */
 	MDP _mdp;
+	/**
+	 * The q-table to store values in
+	 */
 	QTable _qtable;
+	/**
+	 * A table to keep track of how many times a state has gone through a roll-out
+	 */
 	SCountTable _s_counts;
 	
+	/**
+	 * discount rate
+	 */
 	Reward _gamma;
+	/**
+	 * unused at the moment
+	 */
 	Reward _epsilon;
 	
+	/**
+	 * Max number of times a state can be in a roll-out
+	 */
 	Index _m;
 	
+	/**
+	 * Max number of roll-outs per call to getAction
+	 */
 	Size _run_limit;
+	/**
+	 * Max number milliseconds per call to getAction
+	 */
 	time_t _time_limit;
 	
+	/**
+	 * Likelihood that a roll-out explores a random action
+	 */
 	Probability _explore_epsilon;
+	/**
+	 * Max depth for a roll-out
+	 */
 	Size _max_depth;
 	
+	/**
+	 * used for its ability to perform Bellman backups
+	 */
 	VIPlanner _vi_planner;
 	
-	Reward runSimulation(const State& s, StateSet ss, Size depth=0);
-	
+	/**
+	 * Perform a simulation/roll-out starting at s. Returns the biggest
+	 * Bellman residual.
+	 */
+	Reward runSimulation(const State& s, Size depth=0);
 	
 	_RTDPPlanner(Domain domain, MDP mdp, QTable qtable, SCountTable s_counts,
 	             Reward gamma, Reward epsilon, Index m,
@@ -44,10 +86,17 @@ public:
 	void setRunLimit(Size run_limit) {_run_limit=run_limit;}
 	void setTimeLimit(time_t time_limit) {_time_limit=time_limit;}
 	
+	/**
+	 * from _Planner
+	 */
 	virtual Action getAction(const State& s);
 };
 typedef boost::shared_ptr<_RTDPPlanner> RTDPPlanner;
 
+/**
+ * This is a subclass that provides the necessary data structures. This
+ * version of RTDPPlanner provides a flat q-table and state count table.
+ */
 class _FlatRTDPPlanner : public _RTDPPlanner {
 public:
 	_FlatRTDPPlanner(Domain domain, MDP mdp,
--- libcrl/src/rtdp.cpp
+++ libcrl/src/rtdp.cpp
@@ -6,17 +6,16 @@ using namespace std;
 using namespace cpputil;
 using namespace crl;
 
-Reward _RTDPPlanner::runSimulation(const State& s, StateSet ss, Size depth) {
+Reward _RTDPPlanner::runSimulation(const State& s, Size depth) {
 	if (depth >= _max_depth)
 		return 0;
 		
-//	ss->insert(s);
-
+	//if we've simulated this state too often, terminate the roll-out
 	Index count = _s_counts->getValue(s);
 	if (count >= _m)
 		return 0;
-//	cerr << count << endl;
-		
+	
+	//select an epsilon greedy action
 	Action a = _qtable->getBestAction(s);
 	if (randDouble() < _explore_epsilon) {
 		a = Action(_domain, random()%_domain->getNumActions());
@@ -25,19 +24,24 @@ Reward _RTDPPlanner::runSimulation(const State& s, StateSet ss, Size depth) {
 	Reward error = 0;
 	
 	try {
+		//sample from the MDP
 		State n = _mdp->T(s, a)->sample();
 		
-		Reward next_error = runSimulation(n, ss, depth+1);
+		Reward next_error = runSimulation(n, depth+1);
 		
+		//use the VI planner to perform a backup on s
 		ActionIterator aitr = _mdp->A();
 		error = _vi_planner->backupState(s, aitr);
+		//update the simulation count for this state
 		_s_counts->setValue(s, count+1);
+		
 		if (next_error > error)
 			error = next_error;
-			
 	}
 	catch (DistributionException e) {
-//		cerr << "reached terminal state" << endl;
+		/* reached terminal state.
+		 * sampling throws an exception if all next states have probability 0.
+		 */
 	}
 	return error;
 }
@@ -47,15 +51,12 @@ Action _RTDPPlanner::getAction(const State& s) {
 	time_t start_time = time_in_milli();
 	time_t time_total = 0;
 	
-//	if (_s_counts->getValue(s) < _m) {
-	while (_s_counts->getValue(s) < _m && (_run_limit==0 || num_runs<_run_limit) && (_time_limit==0 || time_total<_time_limit)) {
+	while (_s_counts->getValue(s) < _m &&
+          (_run_limit==0 || num_runs<_run_limit) && //0 for limit means unlimited
+          (_time_limit==0 || time_total<_time_limit)) {
 		
-		StateSet ss(new _StateSet());
-		Reward error = runSimulation(s, ss);
+		Reward error = runSimulation(s);
 		
-//		StateIterator sitr = StateIterator(new _StateSetIterator(*ss));
-//		ActionIterator aitr = _mdp->A();
-		//_vi_planner->plan(sitr, aitr);
 		time_total = time_in_milli()-start_time;
 		num_runs++;
 		
@@ -67,15 +68,15 @@ Action _RTDPPlanner::getAction(const State& s) {
 	//always back up the queried state
 	ActionIterator aitr = _mdp->A();
 	_vi_planner->backupState(s, aitr);
-//	}
-	
-//	cerr << num_runs << " " << _run_limit << endl;
 	
 	Action a = _qtable->getBestAction(s);
-//	cerr << s << "->" << a << endl;
 	return a;
 }
 
+/**
+ * Initialize parameters, as well as created a VI planner using the
+ * provided q-table.
+ */
 _RTDPPlanner::_RTDPPlanner(Domain domain, MDP mdp, QTable qtable,
 						   SCountTable s_counts,
 				           Reward gamma, Reward epsilon, Index m,
@@ -85,10 +86,13 @@ _RTDPPlanner::_RTDPPlanner(Domain domain, MDP mdp, QTable qtable,
   _run_limit(0), _time_limit(0),
   _explore_epsilon(explore_epsilon), _max_depth(max_depth),
   _vi_planner(new _VIPlanner(mdp, epsilon, gamma, _qtable)) {
-//     _vi_planner->plan();
-//     _qtable->print(cerr, _mdp->S(), _mdp->A());
+
 }
 
+/**
+ * This constructor initializes flat q-table and count table, as well
+ * as sending up the planner parameters.
+ */
 _FlatRTDPPlanner::_FlatRTDPPlanner(Domain domain, MDP mdp,
 					               Reward gamma, Reward epsilon, Index m,
 					               Probability explore_epsilon, Size max_depth)
