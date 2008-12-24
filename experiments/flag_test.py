57e360180f8e5e72f9d97ab5020680874e6b4ce7
John Asmuth <jasmuth@gmail.com>
2008-12-24 19:08:50 +0000

    added a test script for flag mazes

---
+++ experiments/flag_test.py
@@ -0,0 +1,21 @@
+experiment_name = "flag_test"
+
+log_dir = "exp_log"
+
+maze = "data/small_rooms.xml"
+cfg = "cfgs/slip001.xml"
+cwd = "/home/jasmuth/crl/mazes"
+agent_echo = Agent("echo RUN YOUR AGENT")
+agent_rtdp1 = Agent("expert_maze_agent "+maze+" "+cfg+" rtdp 1 0.1 25 500 10 0", cwd=cwd)
+agent_vi = Agent("expert_maze_agent "+maze+" "+cfg+" vi 1 .001", cwd=cwd)
+agent_ps = Agent("expert_maze_agent "+maze+" "+cfg+" ps 1 .001", cwd=cwd)
+
+env = Environment("glue_maze_env "+maze+" "+cfg, cwd=cwd)
+
+#instance names must be unique
+instances = [
+    Instance("VI", agent_vi, env, trials=100, runs=1),
+    #Instance("echo", agent_echo, env, trials=100, runs=1),
+    #Instance("PS", agent_ps, env, trials=1000, runs=3),
+    Instance("RTDP_25", agent_rtdp1, env, trials=100, runs=1),
+]
