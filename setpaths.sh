d5f5d2af06b24bc6ba3f0a06f813d4228242bf76
Philipp Robbel <robbel@gmail.com>
2015-02-26 16:23:20 -0800

    [libcrlext] fixes to SConstruct files across directories. Example setpaths.sh to work on local machine.

---
+++ setpaths.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+#
+# source with ". ./setpaths.sh" or "source ./setpaths.sh"
+#
+export CPATH=$CPATH:/home/philipp/Builds/maastricht/3rdParty/csolvers/libcrlext/libcrl/include:/home/philipp/Builds/maastricht/3rdParty/csolvers/libcrlext/rlgnm/include:/home/philipp/Builds/maastricht/3rdParty/csolvers/libcrlext/glue-crl/include
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/philipp/Builds/maastricht/3rdParty/csolvers/libcrlext/libcrl/lib:/home/philipp/Builds/maastricht/3rdParty/csolvers/libcrlext/rlgnm/lib:/home/philipp/Builds/maastricht/3rdParty/csolvers/libcrlext/glue-crl/lib
