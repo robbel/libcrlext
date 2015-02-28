9bd9b32ba61049241ce5104aca1832588fd6ed11
Philipp Robbel <robbel@gmail.com>
2015-02-27 16:03:13 -0800

    setpaths includes /usr/local/lib

--- setpaths.sh
+++ setpaths.sh
@@ -6,4 +6,4 @@
 # gzstream installed.
 #
 export CPATH=$CPATH:/home/philipp/Builds/maastricht/3rdParty/csolvers/libcrlext/libcrl/include:/home/philipp/Builds/maastricht/3rdParty/csolvers/libcrlext/rlgnm/include:/home/philipp/Builds/maastricht/3rdParty/csolvers/libcrlext/glue-crl/include
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/philipp/Builds/maastricht/3rdParty/csolvers/libcrlext/libcrl/lib:/home/philipp/Builds/maastricht/3rdParty/csolvers/libcrlext/rlgnm/lib:/home/philipp/Builds/maastricht/3rdParty/csolvers/libcrlext/glue-crl/lib
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/philipp/Builds/maastricht/3rdParty/csolvers/libcrlext/libcrl/lib:/home/philipp/Builds/maastricht/3rdParty/csolvers/libcrlext/rlgnm/lib:/home/philipp/Builds/maastricht/3rdParty/csolvers/libcrlext/glue-crl/lib:/usr/local/lib
