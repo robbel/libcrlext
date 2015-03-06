e5591f167ec8437fb7c07a48460e0002202dc2b4
Philipp Robbel <robbel@gmail.com>
2015-03-06 13:32:41 -0800

    adding some lgpl license headers.

--- libcrl/src/conversions.cpp
+++ libcrl/src/conversions.cpp
@@ -1,7 +1,12 @@
 /*
     Copyright 2015 Philipp Robbel
 
-    TODO: ADD LICENSE
+    License: http://www.gnu.org/licenses/lgpl LGPL version 3.0, or (at your option) any later version.
+
+    The software is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
  */
 
 #include <iostream>
