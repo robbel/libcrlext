fd300f90e67dd47862ac3ae4a76f4ac2518c3f03
Philipp Robbel <robbel@gmail.com>
2015-03-06 16:04:01 -0800

    [libcrl] including a conversion from factored to flat MDP

--- libcrl/src/test_spudd.cpp
+++ libcrl/src/test_spudd.cpp
@@ -61,6 +61,27 @@ FactoredMDP makeFactoredMDP(Domain domain) {
   return fmdp;
 }
 
+MDP convertToMDP(FactoredMDP fmdp) {
+
+  const Domain& domain = fmdp->getDomain();
+  _FMDP mdp(domain);
+
+  for (Size state_index=0; state_index<domain->getNumStates(); state_index++) {
+          State s(domain, state_index);
+          for (Size action_index=0; action_index<domain->getNumActions(); action_index++) {
+                  Action a(domain, action_index);
+                  mdp.setR(s, a, fmdp->R(s, a));
+                  for (Size next_index=0; next_index<domain->getNumStates(); next_index++) {
+                          State n(domain, next_index);
+                          Probability p = fmdp->T(s,a,n);
+                          mdp.setT(s, a, n, p);
+                  }
+          }
+  }
+
+  return boost::make_shared<_FMDP>(mdp);
+}
+
 int main()
 {
   try {
