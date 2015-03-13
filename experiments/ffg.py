368f3f7dbf44b3e0bcbca14a24494d778d2641ff
Philipp Robbel <robbel@gmail.com>
2015-03-12 17:20:25 -0700

    running first experiments with random agent.

---
+++ experiments/ffg.py
@@ -0,0 +1,39 @@
+experiment_name = "ffg"
+
+log_dir = "exp_log"
+
+# agent_crmax_m10 = Agent("agent_cluster 5 1 .1 10 1 1 0 500 100 5 2 1", cwd="/home/jasmuth/crl/cluster-boss")
+# agent_boss_m10 =    Agent("agent_cluster 5 1 .1 10 1 1 0 500 100 5 2 0", cwd="/home/jasmuth/crl/cluster-boss")
+agent_echo = Agent("echo run an agent")
+
+# agent_rmax2 = Agent("agent_vi_rmax 2 1 .1")
+# agent_rmax5 = Agent("agent_vi_rmax 5 1 .1")
+# agent_rmax10 = Agent("agent_vi_rmax 10 1 .1")
+# agent_rmax20 = Agent("agent_vi_rmax 20 1 .1")
+
+agent_random = Agent("random_agent", cwd="/home/philipp/Builds/maastricht/3rdParty/libcrlext/glue-crl/bin")
+
+env_ffg1 = Environment("env_ffg cfgs/ffg1.xml", cwd="/home/philipp/Builds/maastricht/3rdParty/libcrlext/glue-environments")
+env_littman1 = Environment("env_littman cfgs/littman1.xml", cwd="/home/philipp/Builds/maastricht/3rdParty/libcrlext/glue-environments")
+env_echo = Environment("echo run an environment")
+
+
+num_steps = 200
+num_trials = 50
+num_runs = 1
+
+#instance names must be unique
+instances = [
+
+    #Instance("BOSS_a5_m20", agent_boss_a5_m20, env_littman, steps=num_steps, trials=num_trials, runs=num_runs),
+    #Instance("BOSS_a5_m10", agent_boss_a5_m10, env_littman, steps=num_steps, trials=num_trials, runs=num_runs),
+    #Instance("BOSS_m10", agent_boss_m10, env_littman1, steps=num_steps, trials=num_trials, runs=num_runs),
+    #Instance("CRMAX_m10", agent_crmax_m10, env_littman1, steps=num_steps, trials=num_trials, runs=num_runs),
+    #Instance("BOSS_a5_m2", agent_boss_a5_m2, env_littman, steps=num_steps, trials=num_trials, runs=num_runs),
+    Instance("RAND", agent_random, env_littman1, steps=num_steps, trials=num_trials, runs=num_runs),
+    #Instance("RMAX_m2", agent_rmax2, env_littman, steps=num_steps, trials=num_trials, runs=num_runs),
+    #Instance("RMAX_m5", agent_rmax5, env_littman, steps=num_steps, trials=num_trials, runs=num_runs),
+    #Instance("RMAX_m10", agent_rmax10, env_littman1, steps=num_steps, trials=num_trials, runs=num_runs),
+    #Instance("RMAX_m20", agent_rmax20, env_littman, steps=num_steps, trials=num_trials, runs=num_runs),
+    #Instance("DEBUG", agent_echo, env_littman1, steps=num_steps, trials=num_trials, runs=num_runs),
+]
--- setpaths.sh
+++ setpaths.sh
@@ -7,3 +7,5 @@
 #
 export CPATH=$CPATH:/home/philipp/Builds/maastricht/3rdParty/libcrlext/libcrl/include:/home/philipp/Builds/maastricht/3rdParty/libcrlext/rlgnm/include:/home/philipp/Builds/maastricht/3rdParty/libcrlext/glue-crl/include
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/philipp/Builds/maastricht/3rdParty/libcrlext/libcrl/lib:/home/philipp/Builds/maastricht/3rdParty/libcrlext/rlgnm/lib:/home/philipp/Builds/maastricht/3rdParty/libcrlext/glue-crl/lib:/usr/local/lib
+export PATH=$PATH:/home/philipp/Builds/maastricht/3rdParty/libcrlext/glue-crl/bin:/home/philipp/Builds/maastricht/3rdParty/libcrlext/glue-environments/bin:/home/philipp/Builds/maastricht/3rdParty/libcrlext/mazes/bin:/home/philipp/Builds/maastricht/3rdParty/libcrlext/experimentor/bin
+export PYTHONPATH=$PYTHONPATH:.:/home/philipp/Builds/maastricht/3rdParty/libcrlext/experimentor/lib
