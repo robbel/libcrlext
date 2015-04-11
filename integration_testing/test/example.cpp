60477049bca12379de8ab13ed045b37044c8ecaf
Philipp Robbel <robbel@gmail.com>
2015-04-11 13:03:37 -0700

    [libcrl] adding integration_test stack for testing across multiple packages.

---
+++ integration_testing/test/example.cpp
@@ -0,0 +1,27 @@
+/*
+    Copyright 2015 Philipp Robbel
+
+    License: http://www.gnu.org/licenses/lgpl LGPL version 3.0, or (at your option) any later version.
+
+    The software is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+ */
+
+#include <gtest/gtest.h>
+
+#include "cpputil.hpp"
+#include "crl/crl.hpp"
+
+using namespace std;
+using namespace crl;
+using namespace cpputil;
+
+namespace {
+
+} // anonymous ns
+
+TEST(ExampleTest, EmptyExampleTest) {
+  SUCCEED();
+}
