b15ad816038905122a31a39c8ea2e9b3aae105e4
Philipp Robbel <robbel@gmail.com>
2015-04-27 12:10:07 -0700

    [experiments] adding spudd agent experiment.

---
+++ experiments/sysadmin.py
@@ -0,0 +1,23 @@
+experiment_name = "sysadmin"
+
+log_dir = "exp_log"
+
+agent_echo = Agent("echo run an agent")
+agent_random = Agent("random_agent");
+agent_spudd = Agent("glue_spudd SPUDD-OPTDual-64bit-nonportable.ADD", cwd="/home/philipp/Builds/maastricht/3rdParty/csolvers/libcrlext/libcrl")
+
+env_echo = Environment("echo run an environment")
+glue_sysadmin1 = Environment("glue_sysadmin ring 4")
+
+
+
+num_steps = 200
+num_trials = 50
+num_runs = 1
+
+#instance names must be unique
+instances = [
+    #Instance("DEBUG", agent_echo, env_echo, steps=num_steps, trials=num_trials, runs=num_runs),
+    Instance("RAND", agent_random, glue_sysadmin1, steps=num_steps, trials=num_trials, runs=num_runs),
+    Instance("SPUDD", agent_spudd, glue_sysadmin1, steps=num_steps, trials=num_trials, runs=num_runs),
+]
