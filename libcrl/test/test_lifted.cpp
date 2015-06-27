097791850cc2b34424b9402b15faa5700878a39c
Philipp Robbel <robbel@gmail.com>
2015-06-27 15:06:38 -0700

    [libcrl] lifted test stub.

---
+++ libcrl/test/test_lifted.cpp
@@ -0,0 +1,31 @@
+/*
+    Copyright 2015 Philipp Robbel
+
+    License: http://www.gnu.org/licenses/lgpl LGPL version 3.0, or (at your option) any later version.
+
+    The software is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+ */
+
+#include <gtest/gtest.h>
+
+#include "crl/factor_learner.hpp"
+#include "logger.hpp"
+
+using namespace std;
+using namespace crl;
+using namespace cpputil;
+
+namespace {
+
+//
+// Test fixtures
+//
+
+} // anonymous ns
+
+TEST(LiftedTest, BasicLiftedTest) {
+  SUCCEED();
+}
