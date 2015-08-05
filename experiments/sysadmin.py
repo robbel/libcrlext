9d529c8855dd57708031caaece67f320d6855fd7
Philipp Robbel <robbel@gmail.com>
2015-08-04 21:59:32 -0700

    [experiments] minor mod experiments script.

--- experiments/sysadmin.py
+++ experiments/sysadmin.py
@@ -7,7 +7,7 @@ agent_random = Agent("random_agent");
 agent_null = Agent("null_agent");
 agent_spudd = Agent("glue_spudd ../experiments/agent_spudd/SPUDD-OPTDual-ma-sysadmin-ring-6.ADD", cwd="/home/philipp/Builds/maastricht/3rdParty/libcrlext/libcrl")
 #agent_alp = Agent("glue_alp ring 6");
-agent_alp1 = Agent("glue_alp ring 6 -w integration_testing/bin/weights0.txt", cwd="/home/philipp/Builds/maastricht/3rdParty/libcrlext")
+agent_alp1 = Agent("glue_alp ring 6 -w integration_testing/bin/weights10.txt -b integration_testing/bin/conj10.txt", cwd="/home/philipp/Builds/maastricht/3rdParty/libcrlext")
 
 env_echo = Environment("echo run an environment")
 glue_sysadmin1 = Environment("glue_sysadmin ring 6")
